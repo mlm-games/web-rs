79085a22afe239ed5c213c6ba72895e0f7b60d19
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-05-26 21:36:42 +0530

    fomratting cleanup from pr

--- web-streams/src/reader.rs
+++ web-streams/src/reader.rs
@@ -37,8 +37,6 @@ impl<T: JsCast> Reader<T> {
 		let result: ReadableStreamReadResult = self.read.as_mut().unwrap().await?.into();
 		self.read.take(); // Clear the promise on success
 
-		//todo why do you use `Reflect` here?
-		// is get_done and get_value not good enough?
 		if Reflect::get(&result, &"done".into())?.is_truthy() {
 			return Ok(None);
 		}
@@ -67,77 +65,56 @@ impl<T: JsCast> Drop for Reader<T> {
 	}
 }
 
-
-use wasm_bindgen::JsCast;
-
 #[cfg(feature = "tokio")]
 mod tokio_impl {
-	use std::io::{Result, Error, ErrorKind, ErrorKind::Unsupported};
 	use super::*;
+	use crate::reader::js_sys::Uint8Array;
+	use js_sys::Promise;
+	use std::future::Future;
+	use std::io::Result;
+	use std::io::{Error, ErrorKind};
 	use std::pin::Pin;
 	use std::task::{Context, Poll};
 	use tokio::io::{AsyncRead, ReadBuf};
-	use wasm_bindgen::JsCast;
-	use crate::reader::js_sys::Uint8Array;
-	use std::future::Future;
-	use Poll::{Pending, Ready};
-	use js_sys::Promise;
 	use ErrorKind::Other;
-	use tracing::info;
+	use Poll::{Pending, Ready};
 
 	impl AsyncRead for Reader<Uint8Array> {
-
-		fn poll_read(
-			mut self: Pin<&mut Self>,
-			cx: &mut Context<'_>,
-			buf: &mut ReadBuf<'_>,
-		) -> Poll<Result<()>> {
-
+		fn poll_read(mut self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut ReadBuf<'_>) -> Poll<Result<()>> {
 			//if there is no pending read, we need to create one
 			if self.read.is_none() {
 				self.read = Some(JsFuture::from(self.inner.read()));
 			}
 
-			let Some(promise) =  self.read.as_mut() else {
-				return Ready(Err(Error::new(Other, "Unrecoverable error: No pending read found despite just queued")));
+			let Some(promise) = self.read.as_mut() else {
+				return Ready(Err(Error::new(Other, "no pending read found")));
 			};
 
 			match Pin::new(promise).poll(cx) {
 				Pending => Pending,
 				Ready(Ok(js_val)) => {
-					//we clone, set and then take here because
-					//in case of pending it needs to be in read
 					self.read.take();
 
-					//it seems at the moment that the value cannot be typechecked?
 					let result = js_val.unchecked_into::<ReadableStreamReadResult>();
-					/*let Ok(result) = js_val.dyn_into::<ReadableStreamReadResult>() else {
-						return Ready(Err(Error::new(Unsupported, "Unrecoverable error: Expected js type ReadableStreamReadResult")));
-					};*/
 					if result.get_done().unwrap_or(false) {
 						return Ready(Ok(())); // EOF
 					}
 
 					let Ok(array) = result.get_value().dyn_into::<Uint8Array>() else {
-						return Ready(Err(Error::new(Unsupported, "Unrecoverable error: Expected js type Uint8Array")));
+						return Ready(Err(Error::new(Other, "expected js Uint8Array")));
 					};
 					let array_len = array.length() as usize;
 					let len = std::cmp::min(buf.remaining(), array_len);
 
-					// Copy what fits
-					// # Safety: copy_to_uninit does not uninit anything and inits the first `len` bytes.
-					let dst = unsafe {
-						&mut buf.unfilled_mut()[0..len]
-					};
+					let dst = unsafe { &mut buf.unfilled_mut()[0..len] };
 					array.slice(0, len as u32).copy_to_uninit(dst);
-					unsafe { buf.assume_init(len); }
+					unsafe {
+						buf.assume_init(len);
+					}
 					buf.advance(len);
 
-					// If there are leftover bytes, we must not drop them
-					// create a new ReadableStreamReadResult and set self.read
 					if len < array_len {
 						let leftover = array.slice(len as u32, array_len as u32);
-						//let result = ReadableStreamReadResult::new(); i believe we can reuse the existing one
 						result.set_done(false);
 						result.set_value(&**leftover);
 						let promise = Promise::resolve(&**result);
@@ -154,4 +131,3 @@ mod tokio_impl {
 		}
 	}
 }
-
